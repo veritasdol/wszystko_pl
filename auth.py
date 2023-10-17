538462c1e6bdc284a9d6a1a2b8abe73b943ba4bc
veritasdol <veritas.dol@gmail.com>
2023-10-17 10:10:07 +0200

    add json validation and some optimization

--- auth.py
+++ auth.py
@@ -1,31 +1,36 @@
-from os import environ, path
 import requests
 import json
+import pydantic
 from datetime import datetime, timedelta
+from typing import Optional
 
 
 API_URL = 'https://wszystko.pl/api'
-DEVICE_CODE_VAR_NAME = 'WSZYSTKO_PL_DEVICE_CODE'  # type your variable name
 TOKEN_JSON_FILE = 'tokens.json'  # type path to your tokens file
 
 
-def get_device_code(is_registered: bool = True) -> str:
+class TokenSchema(pydantic.BaseModel):
+    accessToken: str
+    expiresIn: int
+    refreshExpiresIn: int
+    refreshToken: str
+    tokenType: str
+    date: Optional[str] = None
+
+
+def get_device_code() -> str:
     '''
     Return device code
     '''
-    if is_registered and environ.get(DEVICE_CODE_VAR_NAME):
-        device_code = environ.get(DEVICE_CODE_VAR_NAME)
-        return device_code
-    else:
-        response = send_request(method='GET', path='/integration/register')
-        device_code = response['deviceCode']
-        input(f'Open this URL in your browser and complete authorization: {response["verificationUriPrettyComplete"]} \n Then press Enter')
-        print(
-            f'Add your "Device Code" to a System variables and reboot your system: {device_code}')
-        return device_code
+
+    response = send_request(method='GET', path='/integration/register')
+    device_code = response['deviceCode']
+    input(
+        f'Open this URL in your browser and complete authorization: {response["verificationUriPrettyComplete"]} \n Then press Enter')
+    return device_code
 
 
-def send_request(method:str, path: str, device_code: str = None, refresh_token: str = None) -> dict:
+def send_request(method: str, path: str, device_code: str = None, refresh_token: str = None) -> dict:
     params = {'deviceCode': device_code, 'refreshToken': refresh_token}
     url = API_URL + path
     try:
@@ -36,12 +41,13 @@ def send_request(method:str, path: str, device_code: str = None, refresh_token: 
         return response.json()
     except requests.exceptions.HTTPError as err:
         raise SystemExit(err)
-    
+
 
 def get_token(device_code: str = None, refresh_token: str = None) -> str:
     '''
     Send request to API for new token or token extension
     '''
+
     match device_code:
         case None: response = send_request(
             method='POST',
@@ -64,21 +70,24 @@ def check_file(path_to_file: str) -> bool:
     Return True if file exist and it is not empty, False otherwise.
     '''
 
-    if path.isfile(path_to_file):
+    try:
         with open(path_to_file, 'r') as file:
-            return bool(file.read())
-    return False
+            TokenSchema.model_validate_json(file.read())
+            return True
+    except ValueError or pydantic.ValidationError or FileNotFoundError:
+        return False
 
 
 def check_token(data: dict) -> bool:
     '''
     Return True if token still valid, False otherwise.
     '''
-    
+
     date = datetime.fromisoformat(data['date'])
     token_exp_time = data['expiresIn']
     return date + timedelta(seconds=token_exp_time) > datetime.now()
-    
+
+
 def token() -> str:
     '''
     Return token
@@ -89,7 +98,7 @@ def token() -> str:
             if check_token(data):
                 return data['accessToken']
             return get_token(refresh_token=data['refreshToken'])
-    device_code = get_device_code(is_registered=False)
+    device_code = get_device_code()
     return get_token(device_code=device_code)
 
 
